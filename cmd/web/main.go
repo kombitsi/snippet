aab743d2f337b8b1c18714e35824be978cc98728
Dmitry Kombitsi <dmitrykombitsi@MacBook-Pro-Dmitry.local>
2021-08-05 20:07:30 +0300

    addflagconfig

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+    "flag"
     "log"
     "net/http"
 )
 
 
 func main()  {
+    addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
+
+    flag.Parse()
+
     mux :=http.NewServeMux()
     mux.HandleFunc("/", home)
     mux.HandleFunc("/snippet", showSnippet)
     mux.HandleFunc("/snippet/create", createSnippet)
+
     fileServer := http.FileServer(http.Dir("./ui/static/"))
+    mux.Handle("/static/", http.StripPrefix("/static",fileServer))
 
-
-    log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-    err := http.ListenAndServe(":4000", mux)
+    log.Printf ("Запуск веб-сервера на %s", *addr)
+    err := http.ListenAndServe(*addr, mux)
     log.Fatal(err)
-    
 }
 
