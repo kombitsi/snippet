4dc3417d44191875fb4e92640ca9629f5538378b
Dmitry Kombitsi <dmitrykombitsi@MacBook-Pro-Dmitry.local>
2021-08-06 15:35:02 +0300

    addconfig

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	"html/template"
 	_ "html/template"
-	"log"
-	_ "log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w,r)
 		return
@@ -23,18 +22,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request)  {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request)  {
 	id, err :=strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1{
 		http.NotFound(w,r)
@@ -44,7 +43,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request)  {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request)  {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Метод запрещен", 405)
