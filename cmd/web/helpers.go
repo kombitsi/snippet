8f1f496e992a895327dd699ad4e56bfa8e0b362b
Dmitry Kombitsi <dmitrykombitsi@MacBook-Pro-Dmitry.local>
2021-08-07 20:26:04 +0300

    addserver

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	_ "fmt"
+	"net/http"
+	_ "net/http"
+	"runtime/debug"
+	_ "runtime/debug"
+	_ "strconv"
+)
+
+func (app *application) serverError(w http.ResponseWriter, err error)  {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Println(trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int)  {
+	http.Error(w, http.StatusText(status), status)
+
+}
+
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
