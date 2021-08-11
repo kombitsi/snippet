602aaf466e325f4c9b02bd0e825b030dcf10b6ef
Dmitry Kombitsi <dmitrykombitsi@MacBook-Pro-Dmitry.local>
2021-08-11 18:53:54 +0300

    adddbconnpull

--- cmd/web/mai.go
+++ cmd/web/mai.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "database/sql"
     "flag"
     "log"
     "net/http"
@@ -14,10 +15,20 @@ type application struct {
 
 func main()  {
     addr :=flag.String("addr", ":4000", "Сетевой адрес веб-сервиса")
+
+    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "Название MySQL источника данных")
+
     flag.Parse()
 
     infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
     errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+    db, err := openDB(*dsn)
+    if err != nil {
+        errorLog.Fatal(err)
+    }
+
+    defer db.Close()
 
     app := &application{
         errorLog: errorLog,
@@ -29,6 +40,17 @@ func main()  {
         Handler: app.routes(),
     }
     infoLog.Printf("Запуск сервера на %s", *addr)
-    err := srv.ListenAndServe()
+    err = srv.ListenAndServe()
     errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+    if err !=nil {
+        return nil, err
+    }
+    if err = db.Ping(); err != nil {
+        return nil, err
+    }
+    return db, nil
 }
